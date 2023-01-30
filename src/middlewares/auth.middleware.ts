c63efcf50d28e5463722394659ac940bd666df67
Abaeze Noble <onyiboixy@gmail.com>
2023-01-30 21:15:11 +0100

    auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express';
+import { verifyJwt } from '../utils/helper';
+import { UserService } from '../services/users/users.service';
+export const authMiddleware:RequestHandler =async (req, res, next) => {
+    const token = req.headers.authorization ;
+    const userService = new UserService()
+  
+    if (!token) {
+      return res.status(401).send("Access Denied. No Token Provided.");
+    }
+    const payload =  verifyJwt(token)
+    if (!payload) {
+      return res.status(400).send("Invalid Token.");
+
+    }
+    const user = await userService.findOne(payload.id)
+    req.authUser = user
+    next();
+  }
