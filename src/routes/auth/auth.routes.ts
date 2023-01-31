ff87d9816436b94bf1ccf36c9a8e448f40087eaf
Abaeze Noble <onyiboixy@gmail.com>
2023-01-31 22:46:45 +0100

    auth endpoint

---
+++ src/routes/auth/auth.routes.ts
@@ -0,0 +1,30 @@
+import { createUserSchema } from './../../dtos/user/create-user.dto';
+import { loginSchema } from './../../dtos/auth/login.dto';
+import { createController } from "./../../utils/createController";
+import { Router } from "express";
+import { AppRoutes } from "./../../interfaces/route.interface";
+import { AuthController } from "../../controllers/auth.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
+import validationMiddleware from '../../middlewares/validation.middleware';
+export class AuthRoute implements AppRoutes {
+  path: string="auth";
+  router: Router = Router();
+  private  authController;
+
+  constructor() {
+    this.authController= new AuthController()
+    this.initializeRoutes();
+  }
+  initializeRoutes() {
+    this.router.post(
+      "/register",
+      validationMiddleware(createUserSchema),
+      this.authController.register
+    );
+    this.router.post('/login',
+    validationMiddleware(loginSchema),
+    this.authController.login
+   )
+    this.router.get('/profile',authMiddleware,this.authController.profile )
+  }
+}
